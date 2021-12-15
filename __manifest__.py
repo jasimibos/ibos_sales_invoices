eca3b118f38d65976d3823218c8350213e67b179
jasimibos <jasimibos@gmail.com>
2021-12-15 17:31:06 +0600

    commit

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'iBOS Sales invoices',
+    'version': '1.1',
+    'summary': 'Report',
+    'sequence': 20,
+    'description': """ """,
+    'category': '',
+    'website': '',
+    'images': [],
+    'depends': [],
+    'data': [
+        'reports/report.xml',
+
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
